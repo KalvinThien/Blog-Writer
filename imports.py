63fe79bc413023e66b7002d22e627edbd279ae91
Marcus Eley <marcus.eley@gmail.com>
2023-03-26 20:13:31 -0700

    Deleted unused imports

--- imports.py
+++ imports.py
@@ -3,18 +3,8 @@ import io
 
 from flask import Request
 from flask import Flask, render_template, request, redirect, url_for
-# from google.oauth2._credentials_async import Credentials
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from googleapiclient.discovery import build
-# from google.auth.transport.requests import Request
-# from googleapiclient.http import MediaIoBaseDownload
-# from googleapiclient.http import MediaFileUpload
 
 from datetime import datetime
-
-from langchain import ConversationChain
-from langchain.chains.conversation.memory import ConversationBufferMemory
-from langchain.llms import OpenAIChat
 
 
 def write_file(folder_name, filename, content):
